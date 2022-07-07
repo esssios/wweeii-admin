48f8a0c7550e9c31f05d7ed7d44a5dab0ece6047
esssios <2499300592@qq.com>
2022-07-07 09:22:22 +0800

    集成插件[pinia]

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import "uno.css";
 
 import { createApp } from "vue";
 import { setupRouter } from "@/router";
+import { setupStore } from "@/store";
 import App from "./App.vue";
 
 const app = createApp(App);
 
 setupRouter(app);
+setupStore(app);
 
 app.mount("#app");
