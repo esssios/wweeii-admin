71ac5e770db5a9ff9613c257d74514d98712d743
esssios <2499300592@qq.com>
2022-07-25 14:40:28 +0800

    style:更新登录页背景

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,37 +1,41 @@
 <template>
-  <div flex h-full>
-    <div m-auto bg-gray-100 w-350 flex flex-col items-center border border-gray-300 p-30 rounded-10>
-      <h5 text-24 font-normal color="#6a6a6a">
-        {{ title }}
-      </h5>
-      <div mt-30 w-full>
-        <n-input
-          v-model:value="loginInfo.name"
-          autofocus
-          class="text-16 items-center h-50 pl-10"
-          placeholder="admin"
-          :maxlength="20"
-        >
-        </n-input>
-      </div>
-      <div mt-30 w-full>
-        <n-input
-          v-model:value="loginInfo.password"
-          class="text-16 items-center h-50 pl-10"
-          type="password"
-          show-password-on="mousedown"
-          placeholder="123456"
-          :maxlength="20"
-          @keydown.enter="handleLogin"
-        />
-      </div>
+  <div class="cus-scroll h-full py-15 flex-col overflow-auto bg-cover" :style="{ backgroundImage: `url(${bgImg})` }">
+    <div class="m-auto p-15 flex-center min-w-345 rounded-10 card-shadow bg-white bg-opacity-60">
+      <!-- <div w-380 hidden md:block px-20 py-35>
+        <img src="@/assets/images/login_banner.png" w-full alt="login_banner" />
+      </div> -->
 
-      <div mt-20 w-full>
-        <n-checkbox :checked="isRemember" label="记住我" :on-update:checked="(val) => (isRemember = val)" />
-      </div>
+      <div class="w-320 flex-col px-20 py-35">
+        <h5 class="flex-center text-24 font-normal" color="#6a6a6a"><icon-custom-logo mr-30 text-50 />{{ title }}</h5>
+        <div mt-30>
+          <n-input
+            v-model:value="loginInfo.name"
+            autofocus
+            class="text-16 items-center h-50 pl-10"
+            placeholder="admin"
+            :maxlength="20"
+          >
+          </n-input>
+        </div>
+        <div mt-30>
+          <n-input
+            v-model:value="loginInfo.password"
+            class="text-16 items-center h-50 pl-10"
+            type="password"
+            show-password-on="mousedown"
+            placeholder="123456"
+            :maxlength="20"
+            @keydown.enter="handleLogin"
+          />
+        </div>
 
-      <div mt-20 w-full>
-        <n-button w-full h-50 rounded-5 text-16 type="primary" @click="handleLogin">登录</n-button>
+        <div mt-20>
+          <n-checkbox :checked="isRemember" label="记住我" :on-update:checked="(val) => (isRemember = val)" />
+        </div>
+
+        <div mt-20>
+          <n-button class="w-full h-50 rounded-5 text-16" type="primary" :loading="loging" @click="handleLogin">登录</n-button>
+        </div>
       </div>
     </div>
   </div>
@@ -43,6 +47,7 @@ import { lStorage } from "@/utils/cache";
 import { setToken } from "@/utils/token";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import bgImg from '@/assets/svg/cool-background.svg'
 
 const title = import.meta.env.VITE_APP_TITLE;
 
